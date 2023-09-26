1d86404258760377a1ccdca881856799b37dd890
Lương Phạm Việt Anh <108165794+vanhh59@users.noreply.github.com>
2023-09-27 00:01:52 +0700

    Add files via upload
    Add Data

---
+++ Database/SubjectDATA.sql
@@ -0,0 +1,66 @@
+-- Inserting values into the Subject table for Business Administration
+INSERT INTO Subject (ID, code, status)
+VALUES
+    ('ACC101', 'Principles of Accounting', 1),
+    ('ECO121', 'Macroeconomics', 1),
+    ('ENM301', 'Business English', 1),
+    ('ENM401', 'Business English', 1),
+    ('OBE102c', 'Organizational Behavior', 1),
+    ('ECO201', 'International Economics', 1),
+    ('FIN202', 'Principles of Corporate Finance', 1),
+    ('HRM201c', 'Human Resource Management', 1),
+    ('IBC201', 'Cross Cultural Management and Negotiation', 1),
+    ('IBI101', 'Introduction to International Business', 1),
+    ('IBF301', 'International Finance', 1),
+    ('ITA203c', 'Management Information Systems', 1),
+    ('MAS202', 'Applied Statistics for Business', 1),
+    ('SCM201', 'Supply Chain Management', 1),
+    ('IBS301m', 'International Business Strategy', 1),
+    ('IEI301', 'Import Export', 1),
+    ('MKT205c', 'International Marketing', 1),
+    ('SSB201', 'Advanced Business Communication', 1),
+    ('LAW102', 'Business Law and Ethics Fundamentals', 1);
+
+-- Inserting values into the Subject table for English Studies
+INSERT INTO Subject (ID, code, status)
+VALUES
+    ('EAW211', 'English Academic Writing', 1),
+    ('ECR201', 'Critical Reading', 1),
+    ('ENG302c', 'Advanced English Grammar', 1),
+    ('ENP102', 'English phonetics and phonology in use', 1),
+    ('EAL201', 'Academic Listening', 1),
+    ('EAW221', 'English Academic Writing 2', 1),
+    ('ECB101', 'Culture of English-Speaking Countries', 1),
+    ('LTG202', 'Introduction to Linguistics', 1),
+    ('ERW411', 'Read Think Write 1', 1),
+    ('LIT301', 'British & American Literature', 1),
+    ('SEM101', 'Semantics', 1),
+    ('SSC302c', 'Advanced Presentation Skills', 1),
+    ('ECC301c', 'Cross-cultural Communication', 1),
+    ('EPC301', 'Persuasive Communication', 1);
+
+--Information technology
+INSERT INTO Subject (ID, code, status)
+VALUES
+    ('PRF192', 'Programming Fundamentals', 1),
+    ('MAE101', 'Mathematics for Engineering', 1),
+    ('CEA201', 'Computer Organization and Architecture', 1),
+    ('CSI104', 'Introduction to computing', 1),
+    ('NWC203c', 'Computer Networking', 1),
+    ('PRO192', 'Object-Oriented Programming', 1),
+    ('MAD101', 'Discrete mathematics', 1),
+    ('OSG202', 'Operating Systems', 1),
+    ('CSD201', 'Data Structures and Algorithms', 1),
+    ('DBI202', 'Database Systems', 1),
+    ('LAB211', 'OOP with Java Lab', 1),
+    ('WED201c', 'Web Design', 1),
+    ('IOT102', 'Internet of Things', 1),
+    ('PRJ301', 'Java Web application development', 1),
+    ('SWE201c', 'Introduction to Software Engineering', 1),
+    ('MAS291', 'Probability & statistics', 1),
+    ('ACC101', 'Principles of Accounting', 1),
+    ('SWR302', 'Software Requirements', 1),
+    ('SWT301', 'Software Testing', 1),
+    ('SWP391', 'Software development project', 1),
+    ('ITE302c', 'Ethics in IT', 1);
+
