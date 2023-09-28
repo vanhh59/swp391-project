38ffbdeb66726b3f5f1fb5efd0eb2848ba12c3d8
Nguyễn Quang Khánh <117915893+khaqng@users.noreply.github.com>
2023-09-28 23:13:29 +0700

    Add files via upload

---
+++ Database/ExaminerDATA.sql
@@ -0,0 +1,9 @@
+INSERT INTO [dbo].[Examiner]([ID],[name],[email],[status])
+     VALUES
+           ('EX248','Nguy?n Th? Hoàng','hoangntex248@fe.edu.vn',1),
+		   ('EX165','Lâm H?u Khánh Ph??ng','phuonglhkex165@fe.edu.vn',1),
+		   ('EX392','Tr?n Ng?c Châu','chautnex392@fe.edu.vn',1),
+		   ('EX412','T?ng Quang Hi?u','hieutqex412@fe.edu.vn',1),
+		   ('EX752','V? Th? Thùy D??ng','duongvttex752@fe.edu.vn',1),
+		   ('EX454','Thân Th? Ng?c Vân','vanttnex454@fe.edu.vn',1);
+
