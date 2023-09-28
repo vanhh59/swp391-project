92b29ffd34fab0303e013c17685cf40d285c9b5a
Nguyễn Quang Khánh <117915893+khaqng@users.noreply.github.com>
2023-09-28 22:55:41 +0700

    Add files via upload

---
+++ Database/StudentDATA.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO [dbo].[Student]([ID],[name],[email],[status])
+     VALUES
+           ('SE170525','Hoàng Khánh Duy','duyhkse170525@fpt.edu.vn',1),
+		   ('SE170540','Lương Phạm Việt Anh','anhlpvse170540@fpt.edu.vn',1),
+		   ('SE170545','Nguyễn Quang Khánh','khanhnqse170545@fpt.edu.vn',1),
+		   ('SE171010','Đào Nguyễn Huy Nhân','nhandnhse171010@fpt.edu.vn',1),
+		   ('SE171018','Ngô Gia Huấn','huanngse171018@fpt.edu.vn',1);
+
+
