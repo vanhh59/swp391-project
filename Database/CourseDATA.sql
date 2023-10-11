4c93cc98cc984093806ea8e82b4450529ba47ac9
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-11 21:34:42 +0700

    Create CourseDATA.sql

---
+++ Database/CourseDATA.sql
@@ -0,0 +1,14 @@
+use db_Exam
+
+
+Insert into Course(ID, subjectID,semesterID,name, instructor, status) 
+               values 
+                ('C002', 'ENM301', 'SU25', 'Business English','V? Th? Thùy D??ng', 'true') ;
+				('C003', 'OBE102c','SU25', 'Organizational Behavior','V? Th? Thùy D??ng', 'true') ;
+				('C004', 'ECO201', 'SU25', 'International Economics','Tr?n Ng?c Châu', 'true') ;
+				('C005', 'LAB211', 'SU25', 'OOP with Java Lab','Thân Th? Ng?c Vân', 'true') ;
+				('C006', 'IOT102', 'SU25', 'Internet of Things','T?ng Quang Hi?u', 'true') ;
+				('C007', 'PRJ301', 'SU25', 'Java Web application development','T?ng Quang Hi?u', 'true') ;
+				('C009', 'MAS291', 'SU25', 'Probability & statistics','Nguy?n Th? Hoàng', 'true') ;
+				('C010', 'SWT301', 'SU25', 'Software Testing','Nguy?n Th? Hoàng', 'true') ;
+				('C011', 'SWP391', 'SU25', 'Sofware developement project','Lâm H?u Khánh Ph??ng', 'true') ;
