dbbd6d7a64429e9ed91212282d444f8149e51db6
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-12 06:59:13 +0700

    Create Examiner_In_Semeter.sql

---
+++ Database/Examiner_In_Semeter.sql
@@ -0,0 +1,18 @@
+use  DB_EXAM;
+
+Insert into Examiner_In_Semeter (ID,examinerID,semeterID,totalSlot,minSot,maxSlot,status)
+ values 
+ ('I001','EX165','SU25',25,5,15,'true' );
+ ('I002','EX248','FA23',25,5,15,'true' );
+ ('I003','EX165','FA23',25,5,15,'true' );
+ ('I004','EX165','FA23',25,5,15,'true' );
+ ('I005','EX392','FA23',25,5,15,'true' );
+ ('I006','EX392','FA23',25,5,15,'true' );
+ ('I007','EX412','FA23',25,5,15,'true' );
+ ('I008','EX752','FA23',25,5,15,'true' );
+ ('I009','EX752','FA23',25,5,15,'true' );
+ ('I010','EX454','FA23',25,5,15,'true' );
+ ('I011','EX454','FA23',25,5,15,'true' );
+
+
+
