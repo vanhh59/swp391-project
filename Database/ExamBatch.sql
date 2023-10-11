3ee26f24eaf3a1baa0587607dd4b9c5a0cc9ff2e
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-11 21:47:32 +0700

    Create ExamBatch.sql

---
+++ Database/ExamBatch.sql
@@ -0,0 +1,14 @@
+
+Insert into ExamBatch(ID, CourseID, code, date, location, status)
+ values 
+ (1,'C002','JS1701', 10/11/2023, 'FPTU', 'true');
+ (2,'C002','JS1702', 10/11/2023, 'FPTU', 'true');
+ (3,'C002','JS1703', 10/11/2023, 'FPTU', 'true');
+ (4,'C004','JS1704', 10/11/2023, 'FPTU', 'true');
+ (5,'C005','JS1705', 10/11/2023, 'FPTU', 'true');
+ (6,'C006','JS1706', 10/11/2023, 'FPTU', 'true');
+ (7,'C007','JS1707', 10/11/2023, 'FPTU', 'true');
+ (8,'C009','JS1708', 10/11/2023, 'FPTU', 'true');
+ (9,'C009','JS1709', 10/11/2023, 'FPTU', 'true');
+ (10,'C010','JS1710', 10/11/2023, 'FPTU', 'true');
+
