42d9fc6a592620938ec5c4153af8392a47fc04bf
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-12 10:14:23 +0700

    Update ExaminerDATA.sql

--- Database/ExaminerDATA.sql
+++ Database/ExaminerDATA.sql
@@ -6,3 +6,8 @@ Insert into Examiner(ID,name,email,experienceYears,specialization,status)
 		   ('EX412','T?ng Quang Hi?u','hieutqex412@fe.edu.vn',5,'Infomation Technology',1);
 		   ('EX752','V? Th? Thùy D??ng','duongvttex752@fe.edu.vn',10,'Infomation Technology',1);
 		  ('EX454','Thân Th? Ng?c Vân','vanttnex454@fe.edu.vn',10,'Infomation Technology',1);
+                  ('EX123','Trần Công Đời','Doictex123@fe.edu.vn',7,'Infomation Technology',1);
+		   ('EX124','Đỗ Tấn Nhàn','Nhandtex124@fe.edu.vn',10,'Infomation Technology',1);
+		   ('EX489','Thân Văn Sử','Sutvex489@fe.edu.vn',15,'Infomation Technology',1);
+		   ('EX125','Nguyễn Minh Sang','Sangntex125@fe.edu.vn',9,'Infomation Technology',1);
+		  ('EX126','Hồ Hoàn Kiếm','Kiemhhex126@fe.edu.vn',7,'Infomation Technology',1);
