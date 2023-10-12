1f9d689998ace2c0a46bb4d63ab014ac22556b6f
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-12 10:18:18 +0700

    Create Department.sql

---
+++ Database/Department.sql
@@ -0,0 +1,13 @@
+insert into Department(examinerID,name,examinerType,faculty,location,phone,status)
+values 
+-- ('EX123','Trần Công Đời','Lecture','Information Technology','P701 FPTU Campus','0123456','True');
+-- ('EX124','Đỗ Tấn Nhàn','Lecture','Information Technology','P702, FPTU Campus','098342444546','True');
+ --('EX392','Trần Ngọc Châu','Lecture','Information Technology','P703, FPTU Campus','0983667546','True');
+ --  ('EX412','Tăng Quang Hiếu','Lecture','Information Technology','P704, FPTU Campus','0333427546','True');
+   -- ('EX165','Lâm Hữu Khánh Phương','Lecture','Information Technology','P601, FPTU Campus','0223427546','True');
+	-- ('EX454','Thân Thị Ngọc Vân','Lecture','Information Technology','P501, FPTU Campus','0985527546','True');
+	-- ('EX752','Vũ Thị Thùy Dương','Lecture','Information Technology','P401, FPTU Campus','0988827546','True');
+	-- ('EX489','Thân Văn Sử','Lecture','Information Technology','P301, FPTU Campus','0983427599','True');
+	 --('EX248','Nguyễn Thế Hoàng','Lecture','Information Technology','P201, FPTU Campus','0983427546','True');
+	-- ('EX125','Nguyễn Minh Sang','Lecture','Information Technology','P202, FPTU Campus','0989927546','True');
+	  ('EX126','Hồ Hoàn Kiếm','Lecture','Information Tecnology','P234 FPTU Campus','01245678','True');
