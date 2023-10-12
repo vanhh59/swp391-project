1ab5fd5a997a25fde3de2e04e96a8e6f2d161f50
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-12 11:00:21 +0700

    Create Register.sql

---
+++ Database/Register.sql
@@ -0,0 +1,13 @@
+insert into Register(examinerID,examSlotID, status)
+values 
+ --  ('EX248','Q001','true');
+  -- ('EX165','Q002','true');
+  -- ('EX392','Q003','true');
+  -- ('EX412','Q004','true');
+  -- ('EX752','Q005','true');
+  -- ('EX454','Q006','true');
+ --  ('EX123','Q007','true');
+  -- ('EX124','Q008','true');
+ --  ('EX489','Q009','true');
+  -- ('EX125','Q010','true');
+  -- ('EX126','Q011','true');
