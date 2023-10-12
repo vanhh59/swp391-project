d8fcb8bcc574227b792cb09aa65ba927b8e4e08c
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-12 10:47:11 +0700

    Update StudentDATA.sql

--- Database/StudentDATA.sql
+++ Database/StudentDATA.sql
@@ -5,5 +5,11 @@
 		   ('SE170545','Nguyễn Quang Khánh','khanhnqse170545@fpt.edu.vn',1),
 		   ('SE171010','Đào Nguyễn Huy Nhân','nhandnhse171010@fpt.edu.vn',1),
 		   ('SE171018','Ngô Gia Huấn','huanngse171018@fpt.edu.vn',1);
-
+    Insert into Student(ID, name, email, dateOfBirth,major,yearOfStudy,Status)
+values 
+('SE170525','Hoàng Khánh Duy','duyhkse170525@fpt.edu.vn','09/15/2003','Sofware Engineer',2021,'True');
+('SE170540','Lương Phạm Việt Anh','anhlpvse170540@fpt.edu.vn','10/14/2003','Sofware Engineer',2021,'True');
+('SE170545','Nguyễn Quang Khánh','khanhnqse170545@fpt.edu.vn','08/07/2003','Sofware Engineer',2021,'True');
+('SE171010','Đào Nguyễn Huy Nhân','nhandnhse171010@fpt.edu.vn','02/03/2003','Sofware Engineer',2021,'True');
+('SE171018','Ngô Gia Huấn','huanngse171018@fpt.edu.vn','07/12/2003','Sofware Engineer',2021,'True');
 
