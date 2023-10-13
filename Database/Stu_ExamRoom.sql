5addd850511a312be099b0cd638599b9a1717e5e
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-13 16:05:00 +0700

    Create Stu_ExamRoom.sql

---
+++ Database/Stu_ExamRoom.sql
@@ -0,0 +1,29 @@
+insert into Stu_ExamRoom(studentID,examRoomID,status)
+values 
+  -- ('SE170525','R001','True');
+  -- ('SE170540','R001','True');
+  -- ('SE170545','R001','True');
+  -- ('SE171010','R001','True');
+  -- ('SE171018','R001','True');
+  -- ('SE170525','R002','True');
+  -- ('SE170540','R002','True');
+  -- ('SE170545','R002','True');
+  -- ('SE171010','R002','True');
+  -- ('SE171018','R002','True');
+  -- ('SE170525','R003','True');
+  -- ('SE170540','R003','True');
+  insert into Stu_ExamRoom(studentID,examRoomID,status)
+values 
+ -- ('SE170545','R003','True');
+ --  ('SE171010','R003','True');
+ --  ('SE171018','R003','True');
+ --  ('SE170525','R004','True');
+ --  ('SE170540','R004','True');
+  -- ('SE170545','R004','True');
+  -- ('SE171010','R004','True');
+--   ('SE171018','R004','True');
+  -- ('SE170525','R005','True');
+ --  ('SE170540','R005','True');
+  -- ('SE170545','R005','True');
+--   ('SE171010','R005','True');
+   ('SE171018','R005','True');
