8655967bbb81397e8930c6076cded29af5020535
Hoàng Khánh Duy <144306344+khanhduy123456@users.noreply.github.com>
2023-10-23 11:01:15 +0700

    Update ExaminerDATA.sql

--- Database/ExaminerDATA.sql
+++ Database/ExaminerDATA.sql
@@ -1,11 +1,11 @@
 Insert into Examiner(ID,name,email,experienceYears,specialization,status)
  values 
-           ('EX248','Nguy?n Th? Hoàng','hoangntex248@fe.edu.vn',7,'Infomation Technology',1);
-		   ('EX165','Lâm H?u Khánh Ph??ng','phuonglhkex165@fe.edu.vn',15,'Infomation Technology',1);
-		   ('EX392','Tr?n Ng?c Châu','chautnex392@fe.edu.vn',13,'Infomation Technology',1);
-		   ('EX412','T?ng Quang Hi?u','hieutqex412@fe.edu.vn',5,'Infomation Technology',1);
-		   ('EX752','V? Th? Thùy D??ng','duongvttex752@fe.edu.vn',10,'Infomation Technology',1);
-		  ('EX454','Thân Th? Ng?c Vân','vanttnex454@fe.edu.vn',10,'Infomation Technology',1);
+           ('EX248','Nguyễn Thế Hoàng','hoangntex248@fe.edu.vn',7,'Infomation Technology',1);
+		   ('EX165','Lâm Hữu Khánh Phương','phuonglhkex165@fe.edu.vn',15,'Infomation Technology',1);
+		   ('EX392','Trần Ngọc Châu','chautnex392@fe.edu.vn',13,'Infomation Technology',1);
+		   ('EX412','Tăng Quang Hiếu','hieutqex412@fe.edu.vn',5,'Infomation Technology',1);
+		   ('EX752','Vũ Thị Thùy Dưong','duongvttex752@fe.edu.vn',10,'Infomation Technology',1);
+		  ('EX454','Thân Thị Ngọc Vân','vanttnex454@fe.edu.vn',10,'Infomation Technology',1);
                   ('EX123','Trần Công Đời','Doictex123@fe.edu.vn',7,'Infomation Technology',1);
 		   ('EX124','Đỗ Tấn Nhàn','Nhandtex124@fe.edu.vn',10,'Infomation Technology',1);
 		   ('EX489','Thân Văn Sử','Sutvex489@fe.edu.vn',15,'Infomation Technology',1);
